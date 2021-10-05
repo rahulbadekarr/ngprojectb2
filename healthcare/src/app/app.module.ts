e5b379af6047c1f66d6bbcb8060c915d51558b07
Shrivastavchanchal <chanchal.shrivastava@citiustech.com>
2021-10-05 21:03:58 +0530

    login&registration component added

--- healthcare/src/app/app.module.ts
+++ healthcare/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import {ButtonModule} from 'primeng/button';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/auth/login/login.component';
+import { RegistrationComponent } from './component/auth/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
