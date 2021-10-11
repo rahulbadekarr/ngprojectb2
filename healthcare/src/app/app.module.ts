cf5a7a0408dd60f16a7be494d58b6a1f316c95a4
Shrivastavchanchal <chanchal.shrivastava@citiustech.com>
2021-10-11 23:12:58 +0530

    Angular Mat code added

--- healthcare/src/app/app.module.ts
+++ healthcare/src/app/app.module.ts
@@ -7,6 +7,27 @@ import { AppComponent } from './app.component';
 import { ForgotPasswordComponent } from './component/auth/forgot-password/forgot-password.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// ... Mat files includes here temporarily only
+import { CommonModule } from '@angular/common';
+import {MatSidenavContainer, MatSidenavModule, MatSidenavContent, MatSidenav} from '@angular/material/sidenav';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { MatSliderModule } from '@angular/material/slider';
+import {PortalModule} from '@angular/cdk/portal';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule, MatIcon, MatIconRegistry} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import { A11yModule } from '@angular/cdk/a11y';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { MatNavList } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// ... Mat files includes here temporarily only end
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,8 +38,34 @@ import { AppRoutingModule } from './app-routing.module';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
+  exports:[
+    CommonModule,
+    A11yModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    MatNativeDateModule,
+    MatRippleModule,
+    MatSidenavModule,
+    PortalModule,
+    ScrollingModule,
+    MatSliderModule,
+    MatSidenavContainer,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatNavList,
+    MatSidenavContent, 
+    MatSidenav,
+    MatIcon, 
+    MatIconRegistry
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- healthcare/src/app/component/portal/sidebar/sidebar.component.ts
+++ healthcare/src/app/component/portal/sidebar/sidebar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef,  OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
 
-  constructor() { }
+  fillerNav = Array.from({length: 10}, (_, i) => `Nav Item ${i + 1}`);
 
-  ngOnInit(): void {
+  fillerContent = Array.from({length: 10}, () =>
+      `Chanchshivi212@gmail.com`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
+
