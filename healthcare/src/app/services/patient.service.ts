e4b7c2cee0a56afc130d814601a9ebf47fb5cd65
Abhay Waldia <AbhayW@citiustech.com>
2021-10-13 18:21:37 +0530

    integrated service with patient history component

--- healthcare/src/app/services/patient.service.ts
+++ healthcare/src/app/services/patient.service.ts
@@ -5,12 +5,12 @@ import { Appointments } from 'src/model/tabletypes';
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {
+export class PatientService {
   private baseUrl = 'http://localhost:3004/schedule_appointment';
 
   constructor(private http: HttpClient) {}
 
   getPatientAppoinmentList(userId : string){
-    return this.http.get<Appointments>(`${this.baseUrl}?patient_id=${userId}`);
+    return this.http.get<Appointments[]>(`${this.baseUrl}?patient_id=${userId}`);
   }
 }
