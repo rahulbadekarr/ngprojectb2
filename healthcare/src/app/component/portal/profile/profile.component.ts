5f1062f0dedfe95f1cac43b6589ff6c9a9efd86b
Debasissw <DebasisS@citiustech.com>
2021-10-21 16:38:37 +0530

    profile ts small changes

--- healthcare/src/app/component/portal/profile/profile.component.ts
+++ healthcare/src/app/component/portal/profile/profile.component.ts
@@ -10,6 +10,9 @@ import { Demographics, Users } from 'src/model/tabletypes';
 export class ProfileComponent implements OnInit {
   profile: Demographics = new Demographics();
 user:Users=new Users();
+onFileChanged(event) {
+  const file = event.target.files[0]
+}
 
   constructor(private _userService: UserService) {}
 
@@ -17,6 +20,7 @@ user:Users=new Users();
      this.user = this._userService.getUserDetails();
      this._userService.getUserProfiles(this.user.id).subscribe((data:any) => {
       for(let d of data){
+        // this.user.email = d.email;
         this.profile.dob = d.dob;
         this.profile.gender = d.gender;
         this.profile.phone = d.phone;
