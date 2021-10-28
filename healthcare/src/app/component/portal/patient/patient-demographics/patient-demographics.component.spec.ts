b810173c03cf0a090f2bec7d1f6e25eb02938f21
KarishmaK2 <Karishma.Kubde@citius.com>
2021-10-28 17:54:42 +0530

    changes of unit testing of patient module

--- healthcare/src/app/component/portal/patient/patient-demographics/patient-demographics.component.spec.ts
+++ healthcare/src/app/component/portal/patient/patient-demographics/patient-demographics.component.spec.ts
@@ -1,12 +1,16 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { CustomSnackBarService } from 'src/app/services/snackbar.service';
-import { UserService } from 'src/app/services/user.service';
+import { SharedModule } from 'src/app/component/shared/shared.module';
+import {By} from '@angular/platform-browser';
+
 
 import { PatientDemographicsComponent } from './patient-demographics.component';
+import { CustomSnackBarService } from 'src/app/services/snackbar.service';
+import { UserService } from 'src/app/services/user.service';
 
 describe('PatientDemographicsComponent', () => {
   let component: PatientDemographicsComponent;
@@ -15,8 +19,9 @@ describe('PatientDemographicsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ PatientDemographicsComponent ],
-      imports:[MatSnackBarModule,FormsModule,ReactiveFormsModule,HttpClientTestingModule,RouterTestingModule],
-      providers:[UserService,CustomSnackBarService]
+      imports:[SharedModule,MatSnackBarModule,HttpClientTestingModule,ReactiveFormsModule,RouterTestingModule,BrowserAnimationsModule,],
+	   providers:[UserService,CustomSnackBarService]
+
     })
     .compileComponents();
   });
@@ -25,7 +30,33 @@ describe('PatientDemographicsComponent', () => {
     fixture = TestBed.createComponent(PatientDemographicsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    const de = fixture.debugElement.query(By.css('form'));
   });
+
+  it('form should invalid',async()=> {
+    component.demoForm.controls['firstname'].setValue('');
+    component.demoForm.controls['lastname'].setValue('');
+    expect(component.demoForm.valid).toBeFalsy();
+
+  });
+  it('form should valid',async()=> {
+    component.demoForm.controls['firstname'].setValue('karishma');
+    component.demoForm.controls['lastname'].setValue('kubde');
+    component.demoForm.controls['gender'].setValue('F');
+    component.demoForm.controls['education'].setValue('BE');
+
+
+    expect(component.demoForm.valid).toBeTruthy();
+
+  });
+
+  it('it should submited call onSubmit methode',async () => {
+    fixture.detectChanges();
+    spyOn(component,'onSubmit');
+    const e1 = fixture.debugElement.query(By.css('button')).nativeElement;
+    e1.click();
+    expect(component.onSubmit).toHaveBeenCalledTimes(0);
+   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
