695e5e32fc7c83b9c0f982f1c91efb8910258865
KarishmaK2 <Karishma.Kubde@citius.com>
2021-10-30 12:37:39 +0530

    unit testin of patient immun list page

--- healthcare/src/app/component/portal/patient/patient-immune-list/patient-immune-list.component.spec.ts
+++ healthcare/src/app/component/portal/patient/patient-immune-list/patient-immune-list.component.spec.ts
@@ -5,18 +5,23 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CustomSnackBarService } from 'src/app/services/snackbar.service';
 import { UserService } from 'src/app/services/user.service';
+import {By} from '@angular/platform-browser';
+
 
 import { PatientImmuneListComponent } from './patient-immune-list.component';
+import { DebugElement } from '@angular/core';
 
 describe('PatientImmuneListComponent', () => {
   let component: PatientImmuneListComponent;
   let fixture: ComponentFixture<PatientImmuneListComponent>;
+  let e1 : HTMLElement;
+  let de : DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ PatientImmuneListComponent ],
-      imports:[FormsModule,ReactiveFormsModule,HttpClientTestingModule,RouterTestingModule,MatSnackBarModule],
-      providers:[UserService,CustomSnackBarService]
+      imports:[HttpClientTestingModule,RouterTestingModule,MatSnackBarModule,],
+      providers:[CustomSnackBarService]
     })
     .compileComponents();
   });
@@ -24,8 +29,12 @@ describe('PatientImmuneListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PatientImmuneListComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('form'));
     fixture.detectChanges();
   });
+
+
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
